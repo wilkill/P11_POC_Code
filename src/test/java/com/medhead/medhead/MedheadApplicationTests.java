e0f044c8f7b1c2d690d51314f7e388324f179096
wdalleau <wdalleau@wdalleau-G501JW>
2023-06-19 09:26:34 +0400

    first commit kit starter

---
+++ src/test/java/com/medhead/medhead/MedheadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medhead.medhead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedheadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
