c8f01bbedd3d63f291459f212b201572b4942e3c
wdalleau <wilf974@hotmail.com>
2023-07-22 18:10:14 +0400

    cross-origin fix

---
+++ src/main/java/com/medhead/medhead/backend/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.medhead.medhead.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:4200") // Autoriser toutes les origines (Attention : soyez prudent en production)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+}
